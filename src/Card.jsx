b754abc5f48092c1712201bd922483c739a4a398
Yash Lad <yashlad727@gmail.com>
2022-11-19 21:48:18 +0530

    card class added, how to use props and adding robots data

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,15 +4,20 @@ import React from 'react';
 //     render()
 // }
 
-const Card = () => {
+const Card = (props) => {
+
+    const {name, email, id} = props;
     return (
         // <h1>ROBOFRIENDS</h1>
-        <div>
+        <div className = 'tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <h1>ROBOFRIENDS</h1>
-            <img alt='robots' src = 'https://robohash.org/test?200x200'></img>
+            {/* <img alt='robots' src = 'https://robohash.org/test?200x200'></img> */}
+            <img alt='robots' src = {`https://robohash.org/${id}?200x200`} />
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                {/* <h2>Jane Doe</h2>
+                <p>jane.doe@gmail.com</p> */}
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Card from './Card';
 import 'tachyons';
-
+import { robots } from './robots.js';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-      <Card/>
-      <Card/>
-      <Card/>
+      <Card id = {robots[0].id} name={robots[0].name} email={robots[0].email} />
+      <Card id = {robots[1].id} name={robots[1].name} email={robots[1].email} />
+      <Card id = {robots[2].id} name={robots[2].name} email={robots[2].email} />
     </div>
   </React.StrictMode>
 );
