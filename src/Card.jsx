f014e1d3d077d9d942252077298db92397b25c6f
Yash Lad <yashlad727@gmail.com>
2022-11-19 13:13:53 +0530

    creating cards for RoboFriends

---
+++ src/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// class Card extends React.Component {
+//     render()
+// }
+
+const Card = () => {
+    return (
+        // <h1>ROBOFRIENDS</h1>
+        <div>
+            <h1>ROBOFRIENDS</h1>
+            <img alt='robots' src = 'https://robohash.org/test?200x200'></img>
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.doe@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
