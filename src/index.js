8317e56778bad07e30777553097476175032e662
Yash Lad <yashlad727@gmail.com>
2022-11-20 11:09:53 +0530

    adding more cards and creating function

--- src/index.js
+++ src/index.js
@@ -9,11 +9,6 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <div>
-      <Card id = {robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id = {robots[1].id} name={robots[1].name} email={robots[1].email} />
-      <Card id = {robots[2].id} name={robots[2].name} email={robots[2].email} />
-    </div> */}
     <CardList robots={robots}/>
   </React.StrictMode>
 );
